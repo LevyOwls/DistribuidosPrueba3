36716c4364d7e3ecdff186532ae41273b886a13f
EnvyOwls <48996547+EnvyOwls@users.noreply.github.com>
2020-11-29 01:02:09 -0300

    nuestro primer main (?

---
+++ main.py
@@ -0,0 +1,8 @@
+import preproceso as pp
+import funciones as fp
+import numpy as np
+
+def main():
+    (Xe, Ye, nC, L, particulas, max_iteraciones,C) = pp.preproceso()
+    if __name__== "__main__" :
+        main()    
